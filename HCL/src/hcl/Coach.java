4c315a5fad045a9a3dad06881455c84758c5f8c2
iliasbtk <ilias.betkaoui@gmail.com>
2021-04-13 15:19:32 +0100

    #02 Create and add coaches to the HCL centre

---
+++ HCL/src/hcl/Coach.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hcl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author NGSI
+ */
+public class Coach extends Personnel {
+    
+    Set<String> expertiseArea = new HashSet<> ();
+    private String officeHours;
+    private String officeDay;
+    
+    public Coach(String firstName, String lastName, String address, String tel, String officeHours, String officeDay){
+        super(firstName, lastName, address, tel);
+        this.officeHours = officeHours;
+        this.officeDay = officeDay;
+    }
+    public Coach(String firstName, String lastName, String tel, String officeHours, String officeDay){
+        super(firstName, lastName, tel);
+        this.officeHours = officeHours;
+        this.officeDay = officeDay;
+    }
+    
+    public void addArea(String sport){
+        expertiseArea.add(sport);
+    }
+    
+}
