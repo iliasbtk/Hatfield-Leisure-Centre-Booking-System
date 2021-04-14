e0b7014829b2a0d2f9fb68c530daa4d454f0af27
iliasbtk <ilias.betkaoui@gmail.com>
2021-04-14 11:58:49 +0100

    #08 Cancel a booking

--- HCL/src/hcl/HCL.java
+++ HCL/src/hcl/HCL.java
@@ -246,7 +246,10 @@ public class HCL {
         lesson.decreaseStudentsNumber();
     }
     
-   
+    public void attendLesson (String bookNum){
+        Booking booking = bookings.get(bookNum);
+        booking.setState("Attended");
+    }
     
     
    
