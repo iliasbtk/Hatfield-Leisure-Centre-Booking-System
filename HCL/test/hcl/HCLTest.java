88498c2efbad0d00bd7df236fc3e5d323238cc0b
iliasbtk <ilias.betkaoui@gmail.com>
2021-04-15 17:10:54 +0100

    #11 Unit test for the method addLesson

--- HCL/test/hcl/HCLTest.java
+++ HCL/test/hcl/HCLTest.java
@@ -41,7 +41,7 @@ public class HCLTest {
     
 
 
-    
+   
     @Test
     public void testAddCoach() {
         Coach coach = new Coach("Serita",  "Tuck",  "tel", "16:00 - 17:00", "Mon");
@@ -60,7 +60,7 @@ public class HCLTest {
         hcl.displayCoaches();
 
     }
-    
+   
     @Test
     public void testRegisterStudent() {
         Student student = new Student("Loida", "Poon", "", "");
@@ -68,7 +68,6 @@ public class HCLTest {
         String idS = hcl.registerStudent(student);
         assertEquals(student, (hcl.students).get(idS));
     }
-
 
     @Test
     public void testDisplayStudents() {
@@ -79,17 +78,41 @@ public class HCLTest {
         hcl.registerStudent(student2);
         hcl.displayStudents();
     }
-
-
+    
     @Test
     public void testAddLesson() {
         Coach coach = new Coach("Serita",  "Tuck",  "tel", "16:00 - 17:00", "Mon");
         coach.addArea("swimming");
-        Lesson lesson = new Lesson(coach, "Mon", "14:00 - 15:00", "swimming", "less01");
+        Lesson lesson = new Lesson("Swimming 1", "swimming pool A",coach, "Mon", "14:00 - 15:00", "swimming", 10);
         HCL hcl = new HCL();
         hcl.addLesson(lesson);
         assertEquals(lesson, (hcl.lessons).get(lesson.getId()));
     }
+    
+    /*
+     @Test
+    public void testLookupLessonByArea() {
+        System.out.println("lookupLessonByArea");
+        String area = "";
+        HCL instance = new HCL();
+        instance.lookupLessonByArea(area);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+ 
+    @Test
+    public void testLookupLessonByCoach() {
+        System.out.println("lookupLessonByCoach");
+        String coachName = "";
+        HCL instance = new HCL();
+        instance.lookupLessonByCoach(coachName);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    */
+/**
+    
 
 
     @Test
@@ -100,32 +123,9 @@ public class HCLTest {
   
 
     
-    @Test
-    public void testLookupLessonByArea() {
-        System.out.println("lookupLessonByArea");
-        String area = "";
-        HCL instance = new HCL();
-        instance.lookupLessonByArea(area);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+   
 
-    /**
-     * Test of lookupLessonByCoach method, of class HCL.
-     */
-    @Test
-    public void testLookupLessonByCoach() {
-        System.out.println("lookupLessonByCoach");
-        String coachName = "";
-        HCL instance = new HCL();
-        instance.lookupLessonByCoach(coachName);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
-    /**
-     * Test of book method, of class HCL.
-     */
     @Test
     public void testBook() {
         System.out.println("book");
@@ -137,9 +137,7 @@ public class HCLTest {
         fail("The test case is a prototype.");
     }
 
-    /**
-     * Test of displayBookings method, of class HCL.
-     */
+   
     @Test
     public void testDisplayBookings() {
         System.out.println("displayBookings");
@@ -148,6 +146,6 @@ public class HCLTest {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-
+*/
 
 }
